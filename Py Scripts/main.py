f2c1f3ff5423aa77330a60d3f3a25394f0f977ec
Ahmed-Haitham <ahmed.haitham1997@gmail.com>
2024-06-03 11:30:55 +0200

    Feature_Engineering.py

--- Py Scripts/main.py
+++ Py Scripts/main.py
@@ -8,11 +8,11 @@ import clustering
 def main():
 
     # extract taxi_data.csv
-    filepathcsv = "data/taxi_data.csv"
+    filepathcsv = "../data/taxi_data.csv"
     df = Extract.readcsv(filepathcsv)
 
     # extract nyc.shp
-    filepathshp ="data/nyc-boundaries/geo_export_9ca5396d-336c-47af-9742-ab30cd995e41.shp"
+    filepathshp = "../data/nyc-boundaries/geo_export_9ca5396d-336c-47af-9742-ab30cd995e41.shp"
     nyc = Extract.readshp(filepathshp)
 
     # transform & data cleaning
