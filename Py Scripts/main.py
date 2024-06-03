f1991fb9ddc5b4180ef640066e625ded57da5957
Ahmed-Haitham <ahmed.haitham1997@gmail.com>
2024-06-03 12:27:42 +0200

    updating main.py with Feature Engineering

--- Py Scripts/main.py
+++ Py Scripts/main.py
@@ -4,25 +4,31 @@ import numpy as np
 import Extract
 import Transform
 import clustering
+import Feature_Engineering
 
 def main():
 
     # extract taxi_data.csv
-    filepathcsv = "../data/taxi_data.csv"
-    df = Extract.readcsv(filepathcsv)
+    tripspathcsv = "../data/taxi_data.csv"
+    df = Extract.readcsv(tripspathcsv)
 
     # extract nyc.shp
-    filepathshp = "../data/nyc-boundaries/geo_export_9ca5396d-336c-47af-9742-ab30cd995e41.shp"
-    nyc = Extract.readshp(filepathshp)
+    tripspathshp = "../data/nyc-boundaries/geo_export_9ca5396d-336c-47af-9742-ab30cd995e41.shp"
+    nyc = Extract.readshp(tripspathshp)
 
     # transform & data cleaning
     transformer = Transform.dataTransformation(df,nyc)
     transformedDf = transformer.transform()
 
     # feature engineering
+    transformedDf['trip_distance'] = transformedDf.apply(Feature_Engineering.calculate_distance, axis=1)
+
+    # tempreature data
+    temperature_df = pd.read_csv('../data/NYC_Weather_2016_2022.csv')
+    merged_df = Feature_Engineering.add_temperature(transformedDf, temperature_df)
 
     # clustering
-    cluster = clustering.pickUpCluster(transformedDf)
+    cluster = clustering.pickUpCluster(merged_df)
     df = cluster.clusterCreated()
     print(df)
     print(df['pickup_cluster'].value_counts())
